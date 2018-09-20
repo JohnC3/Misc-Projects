f50f89f23aa6fd9301e868042c83876229a48f34
John Clements <jclements@fb.com>
2018-09-20 14:28:43 -0700

    added unittest template for dijkstras algorithm to save time

--- sorts_from_memory/dijkstras_unittest.py
+++ sorts_from_memory/dijkstras_unittest.py
@@ -1,75 +1,17 @@
 #!/bin/python3
-
-import unittest
 import networkx as nx
-
-
-def pick_next_hop(dist_from_src_dict, visited):
-    # TODO: This is bad
-    shortest_distance = None
-    next_node = None
-    for node, distance in dist_from_src_dict.items():
-        if distance is None or node in visited:
-            continue
-        update = False
-        if shortest_distance is None:
-            update = True
-        elif distance < shortest_distance:
-            update = True
-        if update:
-            next_node = node
-            shortest_distance = distance
-    print('picking ', next_node)
-    return next_node
+import unittest
 
 
 def dijkstras_algorithm(G, src, dest):
-
-    print('hello')
-
-    dist_from_src = {node: None for node in G.nodes()}
-    dist_from_src[src] = 0
-    best_path = {node: [] for node in G.nodes()}
-    best_path[src] = [0]
-    visited = set()
-    unvisited = set(G.nodes())
-
-
-    while len(unvisited) > 0:
-        current_loc = pick_next_hop(dist_from_src, visited)
-        current_dist = dist_from_src[current_loc]
-        unvisited.remove(current_loc)
-        visited.add(current_loc)
-        for neigh in G.neighbors(current_loc):
-            print('checking neighbour', neigh)
-            update = False
-            edge_weight = G.get_edge_data(current_loc, neigh)['weight']
-
-            dist_from_cur = edge_weight + current_dist
-            neigh_dist = dist_from_src[neigh]
-            if neigh_dist is None:
-                update = True
-
-            elif neigh_dist > dist_from_cur:
-                print('assigning, ', neigh, dist_from_cur)
-                update = True
-
-            if update:
-                dist_from_src[neigh] = dist_from_cur
-                path_to_neigh = best_path[current_loc] + [neigh]
-                best_path[neigh] = path_to_neigh
-
-
-
-    return dist_from_src[dest], best_path[dest]
+    pass
 
 
 class TestDijkstraImplementation(unittest.TestCase):
 
     def build_example_graph(self):
-
-        G = nx.Graph()
         """
+        Ascii representation of the graph to run checks on
         .............1........2.......
         ..... (1)---------(5)---(6)...
         ...7../............|..../.....
@@ -80,6 +22,8 @@ class TestDijkstraImplementation(unittest.TestCase):
         ......(2)---------(4).........
         .............2................
         """
+
+        G = nx.Graph()
 
         edges = [
             (0, 1, 7),
@@ -110,8 +54,6 @@ class TestDijkstraImplementation(unittest.TestCase):
         G.add_weighted_edges_from(detour_edges)
         return G
 
-
-
     def test_djkestra(self):
         G = self.build_example_graph()
         shortest_dist, shortest_path = dijkstras_algorithm(G, 0, 6)
@@ -127,6 +69,7 @@ class TestDijkstraImplementation(unittest.TestCase):
         print(shortest_dist, shortest_path)
         self.assertEqual(shortest_dist, 7)
         self.assertEqual(shortest_path, [0, 2, 4, 7, 6])
+
 
 if __name__ == "__main__":
     unittest.main()
